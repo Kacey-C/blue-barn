2e215d26bd22c9e63ead0485703603d4ce7efb79
kacey_c <kaceycreative@gmail.com>
2021-08-12 16:59:16 -0400

    Initial commit

---
+++ wp-content/themes/Divi-Child-Theme/functions.php
@@ -0,0 +1,11 @@
+<?php
+function wpb_add_google_fonts() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
+    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Crimson+Text:ital@0;1&family=Raleway:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap', false ); 
+    }
+     
+    add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+ 
+ 
+//you can add custom functions below this line:
